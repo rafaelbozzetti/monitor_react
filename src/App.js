63e972be4d502de1932c1a99aff2b62ab2a6a476
Rafael Bozzetti <rafael.bozzetti@utech.com.br>
2019-05-17 15:51:20 -0300

    Estrutura criado com o reat-create-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+import Header from './components/Header';
+
+const App = () => (
+      <Header />
+);
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+	<header id="main-header">
+		<select>
+			<option> Ramais </option>
+			<option> Agentes </option>
+			<option> Chamadas </option>
+			<option> Troncos </option>
+			<option> Filas </option>
+			<option> Estacionamentos </option>
+			<option> Gravações </option>
+			<option> Controle de Acesso </option>
+		</select>
+	</header>
+);
+
+export default Header;
